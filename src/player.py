624cbaa8c0bcbb4e757c3ad2012dc8797758140e
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-02 12:56:49 -0700

    Added class

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player:
+    '''Player Class'''
+    def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+    
+    def __str__(self):
+        return f'Player Name: {self.name}\n{self.current_room}'
+    
+    def __repr__(self):
+        return f'Player({repr(self.name)}, {repr(self.current_room)})'
--- src/room.py
+++ src/room.py
@@ -1,2 +1,14 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room:
+    '''Room Class'''
+
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return f'Current Position: {self.name}\nDescription: {self.description}'
+
+    def __repr__(self):
+        return f'Room({repr(self.name)}, {repr(self.description)})'
