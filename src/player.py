d8117e9873bce260afda5b5514f7460ceeba9a32
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-03 15:41:24 -0700

    Added add_item and remove_item functions

--- src/player.py
+++ src/player.py
@@ -5,9 +5,24 @@ class Player:
     def __init__(self, name, current_room):
         self.name = name
         self.current_room = current_room
-    
+        self.player_item = {}
+
+    def add_item(self, player_item):
+        if player_item != {}:
+            for k,v in player_item.items():
+                self.player_item.update({k:v})
+
+    def remove_item(self, r_item_name):
+        del self.player_item[r_item_name]
+        print(f'Player dropped: {r_item_name}')
+
+
     def __str__(self):
-        return f'Player Name: {self.name}\n{self.current_room}'
+        item_list = ''
+        for k,v in self.player_item.items():
+            item_list += f'     {v}\n'
+
+        return f'Player Name: {self.name}\n{self.current_room}\nItems:\n {item_list}'
     
     def __repr__(self):
         return f'Player({repr(self.name)}, {repr(self.current_room)})'
--- src/room.py
+++ src/room.py
@@ -6,9 +6,23 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.room_item = {}
+
+    def add_item(self, room_item):
+        if room_item != {}:
+            for k,v in room_item.items():
+                self.room_item.update({k:v})
+
+    def remove_item(self, r_item_name):
+        print(f'Remove {r_item_name} from {self.name}')
+        del self.room_item[r_item_name]
 
     def __str__(self):
-        return f'Current Position: {self.name}\nDescription: {self.description}'
+        item_list = ''
+        for k,v in self.room_item.items():
+            item_list += f'{v}\n=====\n'
+
+        return f'Current Position: {self.name}\n-----\nDescription: {self.description}\n-----\nRoom Item:\n-----\n{item_list}'
 
     def __repr__(self):
         return f'Room({repr(self.name)}, {repr(self.description)})'
